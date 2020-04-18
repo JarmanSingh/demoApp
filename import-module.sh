516230bf54b8c92c1f1b5c178e127fa5d3a05344
Jarman Singh <jarmansingh@lidolearning.com>
2020-04-18 16:51:24 +0530

    initial commit

---
+++ import-module.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -a ../react-native-fs/FS.common.js node_modules/react-native-fs/
+cp -a ../react-native-fs/*.[mh] node_modules/react-native-fs/
+cp -a ../react-native-fs/*.xcodeproj node_modules/react-native-fs/
+cp -a ../react-native-fs/android/src/main/java/com/rnfs/*.java node_modules/react-native-fs/android/src/main/java/com/rnfs/
